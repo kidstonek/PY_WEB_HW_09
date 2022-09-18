a64eb664eb7bf4cd131baaaa6c9b226eb65fee6c
Andrii V <kidstonek@gmail.com>
2022-09-18 20:05:22 +0300

    new models + seed

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import joinedload
 
-from models import session, Name, Address, Email, Phone
+from models import session, Contact, Address, Email, Phone
 
 
 def get_all_names():
-    names = session.query(Name).options(joinedload("address")).all()
-    for name in names:
-        print(vars(name))
-        print(f'{[f" address is {n.address_ad}" for n in name.address]}')
+    contacts = session.query(Contact).options(joinedload("cont_address")).all()
+    for contact in contacts:
+        print(vars(contact))
+        print(f'{[f" address is {n.address_ad}" for n in contact.cont_address]}')
 
 
 if __name__ == '__main__':
